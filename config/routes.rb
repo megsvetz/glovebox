aa64d5642d6ed92cc79111aff603a26dabd7f94f
Steve Thomas <gamgi1@hotmail.com>
2015-07-08 13:55:59 -0600

    added sub routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,18 @@ Rails.application.routes.draw do
   resources :vehicles do
     resources :repairs
   end
+  resources :cars, controller: 'vehicles', type: 'Car'
+  resources :trucks, controller: 'vehicles', type: 'Truck'
+  resources :boats, controller: 'vehicles', type: 'Boat'
+  resources :trailers, controller: 'vehicles', type: 'Trailer'
+  resources :atvs, controller: 'vehicles', type: 'Atv'
+  resources :motorcycles, controller: 'vehicles', type: 'Motorcycle'
+  resources :waverunners, controller: 'vehicles', type: 'Waverunner'
+  resources :dunebuggies, controller: 'vehicles', type: 'Dunebuggy'
+  resources :oilchanges, controller: 'repairs', type: 'Oilchange'
+  resources :tirerotations, controller: 'repairs', type: 'Tirerotation'
+  resources :inspections, controller: 'repairs', type: 'Inspection'
+  resources :alignments, controller: 'repairs', type: 'Alignment'
   resources :insurances
   resources :registrations
 end
