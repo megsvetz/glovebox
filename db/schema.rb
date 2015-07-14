f9d53b75cf9f557465b1f5857a25ae7118d9df86
Carter Graves <carterg31@gmail.com>
2015-07-14 16:18:24 -0600

    added generic test for insurances
    Change-Id: Iffe18ecfd3e631a06fdb9fed38892b386b28a8d5

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20150713214058) do
+ActiveRecord::Schema.define(version: 20150713230552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,12 +72,9 @@ ActiveRecord::Schema.define(version: 20150713214058) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-
     t.text     "city"
     t.string   "state"
-
     t.string   "zip_code"
-
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
