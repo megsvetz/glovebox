653a694de52d27903ecc6e571a2ebef1e4984dfa
Meg Svetz <megsvetz@gmail.com>
2015-07-17 14:41:10 -0600

    fixed javascript issues
    Change-Id: Icea29cd2ce46fa6284ad7d93c8817d9f336ec07a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require gmaps/google
-//= require underscore
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+//= require gmaps/google
+//= require underscore
