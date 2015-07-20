a45cb0902b96de61ed7e30953f20933dba280f4c
Steve Thomas <gamgi1@hotmail.com>
2015-07-20 09:22:51 -0600

    added default image for vehicle
    Change-Id: I89472cca69a64a11f2bf12d46d47f622b66ad71c

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -5,7 +5,9 @@ class Vehicle < ActiveRecord::Base
 	has_many :repairs
 
 	self.inheritance_column = :type
-  has_attached_file :image, styles: { large: "500x500>", medium: "150x150>", thumb: "50x50>"}
+  has_attached_file :image,
+                    styles: { large: "500x500>", medium: "150x150>", thumb: "50x50>"},
+                    default_url: "default_car.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	def self.type
