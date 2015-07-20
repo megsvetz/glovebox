fbc41932ddc2577a8143af897ac10de255c5de37
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-20 11:18:52 -0600

    added mailers and did some hiding
    Change-Id: Idc937831ecca7369289c88eddb4c316d67d6412a

---
+++ app/mailers/contact.rb
@@ -0,0 +1,29 @@
+class Contact < ApplicationMailer
+  include SendGrid
+  default :from => 'GloveBoxTeam@gmail.com'
+
+  def send_contact_info
+    mail :to => 'GloveBoxTeam@gmail.com',
+         :subject => "Welcome Test :-)",
+         :body => 'This is sample'
+  end
+
+  def send_contact_info_html
+    mail :to => 'GloveBoxTeam@gmail.com',
+         :subject => "test HTML email Subject"
+  end
+
+  def email_with_args(to_email, from_email, subject, body)
+    mail to: to_email,
+         from: from_email,
+         subject: subject,
+         body: body
+  end
+
+  def email_with_args_and_instance_vars(first_name, last_name)
+    @first_name = first_name
+    @last_name = lastname
+    mail :to => "GloveBoxTeam@gmail.com",
+         :subject => "test subject"
+  end
+end
