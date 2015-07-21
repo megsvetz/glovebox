a989192f90bbd31d0c48b71b7940f6e5a301945e
Steve Thomas <gamgi1@hotmail.com>
2015-07-21 13:08:53 -0600

    Added view specs for home page
    Change-Id: I05dcfa1daff555dd8402b2d299e77d791a858755

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -1,8 +1,31 @@
 require 'rails_helper'
 
 feature "home", :js => true do
-  scenario "header" do
+  scenario "home page contains content" do
     visit root_path
     expect(page).to have_content "GLOVEBOX"
+    expect(page).to have_content "Features of Glovebox"
+    expect(page).to have_content "Basic Versus Premium Membership"
+    expect(page).to have_content "The GLOVEBOX Team"
+    expect(page).to have_content "Contact Us"
+  end
+
+  scenario "gas icon takes unsigned in to login page" do
+    visit root_path
+    find(:xpath, "//a/img[@alt='gas']/..").click
+    expect(page).to have_content "Log in"
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Remember me"
+  end
+
+  scenario "sign up link goes to sign up page" do
+    visit root_path
+    click_link('Sign Up')
+    expect(find_field("Email").visible?).to eq(true)
+    expect(find_field("City").visible?).to eq(true)
+    expect(find_field("State").visible?).to eq(true)
+    expect(find_field("Zip code").visible?).to eq(true)
+    expect(find_field("Password").visible?).to eq(true)
+    expect(find_button("Sign up").visible?).to eq(true)
   end
 end
