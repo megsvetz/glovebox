b30f65a09a6dd07bda60bf8f1b4e09d17ee68778
Carter Graves <carterg31@gmail.com>
2015-07-21 14:42:25 -0600

    added navbar sign up and sign in test
    Change-Id: I2cd3fdcd9242f04157acc1e07afc79e2b3b2de6b

---
+++ spec/integration/navbar_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature "navbar", :js => true do
+  scenario "Navbar contains About, Login, Sign up" do
+    visit root_path
+    expect(page).to have_content "About"
+    expect(page).to have_content "Login"
+    expect(page).to have_content "Sign up"
+  end
+  scenario "When click on login goes to login page" do
+    visit root_path
+    click_link "Login"
+    expect(find_field("Email").visible?).to eq(true)
+    expect(find_field("Password").visible?).to eq(true)
+    expect(find_button("Log in").visible?).to eq(true)
+  end
+  scenario "When click on Sign up goes to Sign up  page" do
+    visit root_path
+    click_link "Sign up"
+    expect(page).to have_content "Sign up"
+    expect(find_field("Email").visible?).to eq(true)
+    expect(find_field("City").visible?).to eq(true)
+    expect(find_field("State").visible?).to eq(true)
+    expect(find_field("Zip code").visible?).to eq(true)
+    expect(find_field("Password").visible?).to eq(true)
+    expect(find_button("Sign up").visible?).to eq(true)
+    
+  end
+
+end
+
