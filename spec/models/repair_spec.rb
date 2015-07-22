c6e302a6ec997a3ee45b7ea68aa058139fe27be8
Carter Graves <carterg31@gmail.com>
2015-07-22 12:00:53 -0600

    repair spec model
    Change-Id: Id3a2066baee5b4467a6b3efda7b78b5aee3b10d0

--- spec/models/repair_spec.rb
+++ spec/models/repair_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe Repair, type: :model do
     let(:repair){ FactoryGirl.create(:repair) }
 
  describe "Repair type" do
-  it "Has a valid type" do
+   it "Has a valid type" do
     repair.type == Oilchange || Tirerotation || Inspection || Alignment || Modification || Otherrepair 
-  end
+   end
  end
+
+ context 'relationships' do
+    it 'belongs to a vehicle' do
+      should belong_to :vehicle
+    end
+  end 
 
 
 end
