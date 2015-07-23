021e4de5d3cc25b835a2541416e6a445fe3fb71c
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-23 15:54:50 -0600

    yelp more yelp more yelp
    Change-Id: I8b5dee9fd662d669bce54e8c56c0a8a9e44395ea

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,15 +2,15 @@ class MechanicsController < ApplicationController
   require 'yelp'
 
   def index
-    client = Yelp::Client.new({ consumer_key: ENV['config.consumer_key'],
-                            consumer_secret: ENV['config.consumer_secret'],
-                            token: ENV['config.token'],
-                            token_secret: ENV['config.token_secret']
-                          })
-                          
+    client = Yelp::Client.new({
+      binding.pry
+      consumer_key: ENV['config.consumer_key'],
+      consumer_secret: ENV['config.consumer_secret'],
+      token: ENV['config.token'],
+      token_secret: ENV['config.token_secret']
+    })
     if params[:zip_code]
       @zip = params[:zip_code]
-      redirect_to mechanics_path(@zip)
     else
       @zip = current_user.zip_code
     end
