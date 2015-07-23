8237c97e50bb83964a187c08548cc60647349d03
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-23 14:39:51 -0600

    index page bug fix
    Change-Id: I35a9cce9231da0f363b677019fcf9a3702f9b272

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,4 +1,13 @@
 class MechanicsController < ApplicationController
+
   def index
+    if params[:zip_code]
+      @zip = params[:zip_code]
+      redirect_to mechanics_path(@zip)
+    else
+      @zip = current_user.zip_code
+    end
+    parameters = { term: 'auto repair', limit: 9 }
+    @search = Yelp.client.search(@zip, parameters)
   end
 end
