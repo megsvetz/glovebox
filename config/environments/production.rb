94f62bbcab1c16c61c9ea738e9a1a73dd98457b2
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-23 14:56:21 -0600

    yelp stuff
    Change-Id: Icd9856398d1f63c4e6f4095c538bf44d1e1f363c

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,13 @@ Rails.application.configure do
   :url => ':s3_domain_url',
   :path => '/:class/:attahment/:id_partition/:style/:filename'
 }
+
+Yelp.client.configure do |config|
+ config.consumer_key = ENV['config.cosumer_key']
+ config.consumer_secret = ENV['config.consumer_secret']
+ config.token = ENV['config.token']
+ config.token_secret = ENV['config.token_secret']
+end
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.serve_static_assets = true
