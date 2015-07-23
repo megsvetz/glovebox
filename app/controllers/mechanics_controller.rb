ee9cb465323832daa2bfb66fcedcd8986050e6c3
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-23 16:15:38 -0600

    more yelp testing
    Change-Id: I3cbe397f032d2c44476150d2df876e1417d96811

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,13 +1,7 @@
 class MechanicsController < ApplicationController
-  require 'yelp'
 
   def index
-    client = Yelp::Client.new({
-      consumer_key: ENV['config.consumer_key'],
-      consumer_secret: ENV['config.consumer_secret'],
-      token: ENV['config.token'],
-      token_secret: ENV['config.token_secret']
-    })
+
     if params[:zip_code]
       @zip = params[:zip_code]
     else
--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,8 @@
-if Rails.env.development?
-  Yelp.client.configure do |config|
-   config.consumer_key = ENV['config.consumer_key']
-   config.consumer_secret = ENV['config.consumer_secret']
-   config.token = ENV['config.token']
-   config.token_secret = ENV['config.token_secret']
-  end
+require 'yelp'
+
+Yelp.client.configure do |config|
+ config.consumer_key = ENV['config.consumer_key']
+ config.consumer_secret = ENV['config.consumer_secret']
+ config.token = ENV['config.token']
+ config.token_secret = ENV['config.token_secret']
 end
