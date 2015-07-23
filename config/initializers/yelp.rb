12b5293cacceb2bb9db68ca89eaa7d5ad73d37a6
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-23 15:08:32 -0600

    yelp
    Change-Id: I5b3b094995398fe123d655bb04d66ef736a03a4f

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,5 +1,5 @@
 Yelp.client.configure do |config|
- config.consumer_key = ENV['config.cosumer_key']
+ config.consumer_key = ENV['config.consumer_key']
  config.consumer_secret = ENV['config.consumer_secret']
  config.token = ENV['config.token']
  config.token_secret = ENV['config.token_secret']
