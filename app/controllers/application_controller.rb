e1332483dcc8b858c113dbc95e1c2f3b5eaed543
thebradhimself <Bradley.Hintze@gmail.com>
2015-07-27 08:44:22 -0600

    sign up and sign in redirects to vehicle index page, flash message for successful anything
    Change-Id: Ib11fe985a1904ed73eebc90c1f2450ae226c5c08

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :city, :state, :zip_code, :reminder) }
   end
 
+  def after_sign_in_path_for(resource)
+    vehicles_path #your path
+  end
+
 end
