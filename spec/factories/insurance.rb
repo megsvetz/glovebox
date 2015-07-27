5ed8c35ff3883fdfbf2f8f59d3f026ddd58bf69b
Meg Svetz <megsvetz@gmail.com>
2015-07-27 15:41:48 -0600

    fix factories with new requirements
    Change-Id: I29f49a0b97d9d9cd8fceb7f988c7635a1a6e6b87

--- spec/factories/insurance.rb
+++ spec/factories/insurance.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :insurance do
-  policy_number '12345'
-  company 'allstate'
-  cost 200
-  vehicle
- end
+    policy_number '12345'
+    company 'allstate'
+    cost 200
+    vehicle
+  end
 end
--- spec/factories/repair.rb
+++ spec/factories/repair.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :repair do
     type "Oilchange"
-    repair_description "This is a repair desription"
+    repair_description "This is a repair description"
     repair_date { Faker::Date.between(2.days.ago, Date.today) }
     repair_cost 2000
     vehicle
