a02166889822ea3e159a59663c64be77c5623514
Meg Svetz <megsvetz@gmail.com>
2015-07-28 14:05:37 -0600

    controller testing
    Change-Id: I06138c54fd6d78a4111001d443143b95902357b6

--- spec/helpers/repairs_helper_spec.rb
+++ spec/helpers/repairs_helper_spec.rb
@@ -13,3 +13,19 @@ require 'rails_helper'
 RSpec.describe VehiclesHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+
+module RepairsHelper
+  # Returns a dynamic path based on the provided parameters
+  def sti_repair_path(vehicle, type = "repair", repair = nil, action = nil)
+    send "#{format_action(action)}vehicle_#{format_sti(type, repair)}_path", vehicle, repair
+  end
+
+  def format_sti(type, repair)
+    repair ? "#{type.underscore}" : "#{type.underscore.pluralize}"
+  end
+
+  def format_action(action)
+    action ? "#{action}_" : ""
+  end
+end
